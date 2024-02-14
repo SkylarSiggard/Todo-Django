b08bdb56e9146a978f80fc32ae209d3f7e653661
SkylarSiggard <siggardskylar@gmail.com>
2024-02-14 11:03:20 -0700

    setup

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='index'),
+    path('', views.todo_list, name='list'),
+    path('add_todo/', views.add_todo, name='add_todo'),
+    path('update_todo/', views.update_todo, name='update_todo'),
 ]
--- todoApp/views.py
+++ todoApp/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from .models import Todo
+
+def add_todo(request):
+    if request.method == "POST":
+        title = request.POST.get('todo_title')
+        if title:
+            Todo.objects.create(title=title, status=False)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
-def base(request):
-    return render(request, 'list.html')
+def todo_list(request):
+    todos = Todo.objects.all()
+    return render(request, 'list.html', {'todos': todos})
+
+
+def update_todo(request):
+    if request.method == 'POST':
+        todo = get_object_or_404(Todo, id=request.POST.get('todo_id'))
+        todo.status = not todo.status
+        todo.save()
+
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
